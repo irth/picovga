fd915f73b616c18ccd3cdedaf5a6be1c934ebd45
Wojciech Kwolek <me@irth.pl>
2021-01-31 19:05:06 +0100

    lol

--- src/entry.c
+++ src/entry.c
@@ -1,45 +1,30 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 // Our assembled program:
-#include "hello.pio.h"
+#include "sync.pio.h"
+#include "color.pio.h"
 
-int main() {
-    // Choose which PIO instance to use (there are two instances)
+int main() {   
+    // stdio_init_all();
+
     PIO pio = pio0;
-
-    // Our assembled program needs to be loaded into this PIO's instruction
-    // memory. This SDK function will find a location (offset) in the
-    // instruction memory where there is enough space for our program. We need
-    // to remember this location!
-    uint offset = pio_add_program(pio, &hello_program);
-
-    // Find a free state machine on our chosen PIO (erroring if there are
-    // none). Configure it to run our program, and start it, using the
-    // helper function we included in our .pio file.
+    uint offset = pio_add_program(pio, &sync_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    hello_program_init(pio, sm, offset, 14);
+    sync_program_init(pio, sm, offset, 14);
+
     pio_sm_put_blocking(pio, sm, 490+33);
-    gpio_init(10);
-    gpio_init(11);
-    gpio_init(12);
-    gpio_init(13);
-    gpio_set_dir(10, GPIO_OUT);
-    gpio_set_dir(11, GPIO_OUT);
-    gpio_set_dir(12, GPIO_OUT);
-    gpio_set_dir(13, GPIO_OUT);
-    gpio_put(10, 1);
-    gpio_put(11, 1);
-    gpio_put(12, 1);
-    gpio_put(13, 1);
-    // The state machine is now running. Any value we push to its TX FIFO will
+
+
+    PIO pio_color = pio1;
+    uint offset_color = pio_add_program(pio_color, &color_program);
+    uint sm_color = pio_claim_unused_sm(pio_color, true);
+    color_program_init(pio_color, sm_color, offset_color, 10);
+    // pio_sm_put_blocking(pio_color, sm_color, 40);
+
+     // The state machine is now running. Any value we push to its TX FIFO will
     // appear on the LED pin.
     while (true) {
+            // printf("Hello, %d, %d", pio->irq, pio_color->irq);
         sleep_ms(500);
     }
 }
